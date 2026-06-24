bac4e78834a1a26578eddfe336ecedbdc6792359
yedidya ben haim <bhyedidya@gmail.com>
2026-06-24 12:43:28 +0300

    ProcessReports function implementation - not final

--- Program.cs
+++ Program.cs
@@ -1,34 +1,107 @@
-﻿using System;
-using System.IO;
-
-namespace ReportFileAnalyzer
+﻿namespace ReportFileAnalyzer
 {
+
+    enum ReportTypes { Collect, Analyze, Recon, Intel };
     class Project
     {
-
-        
-        static string[] LoadFile(string filePath)
+        static string[]? LoadFile(string filePath)
         {
             if (File.Exists(filePath))
             {
                 try
                 {
                     string[] alllines = File.ReadAllLines(filePath);
-                    return alllines;
+                    int numOfLine = alllines.Length;
+                    if (numOfLine == 0)
+                    {
+                        Console.WriteLine("Error: File is empty");
+                        return null;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"File loaded: {numOfLine} lines found");
+                        return alllines;
+
+                    }
                 }
                 catch (Exception ex)
                 {
                     Console.WriteLine($"Error reading file: {ex}");
-                    return new string[0];
+                    return null;
                 }
 
             }
             else
             {
-                Console.WriteLine("File not found");
-                return new string[0];
+                Console.WriteLine($"Error: File {Path.GetFileName(filePath)} not found");
+                return null;
             }
         }
+
+
+        static int ProcessReports(string[] fileLines)
+        {
+            foreach (string line in fileLines)
+            {
+                string[] linesplit = line.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+                // Checking that there are 5 values
+                if (linesplit.Length < 5)
+                {
+                    continue;
+                }
+                else
+                {
+                    int validLine = 0;
+
+                    // 1. Check ReportType
+                    if (ReportTypes.TryParse(linesplit[1], true, out ReportTypes ReportType))
+                    {
+                        Console.WriteLine($"Type: {ReportType}");
+                        validLine++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid record: Unknown ReportType type");
+                    }
+
+                    // 2. Check Priority
+                    if (int.TryParse(linesplit[2], out int))
+                    {
+                        Console.WriteLine($"Type: {ReportType}");
+                        validLine++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid record: Unknown ReportType type");
+                    }
+                    
+
+                }
+
+
+
+                //foreach (string linesplits in linesplit)
+                //{
+                //    Console.WriteLine(linesplits);
+                //}
+
+            }
+            return 1;
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -37,11 +110,16 @@ namespace ReportFileAnalyzer
 
         static void Main()
         {
-            string [] alllines = LoadFile("reports.txt");
-            foreach (string line in alllines)
-            {
-                Console.WriteLine(line);
-            }
+            string?[] alllines = LoadFile("reports.txt");
+            int m = ProcessReports(alllines);
+
+
+            //Console.WriteLine(alllines)
+
+            //foreach (string line in alllines)
+            //{
+            //    Console.WriteLine(line);
+            //}
         }
     }
 }
