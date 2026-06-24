5a21351abdbfe94d4ac574eea89a393dbee79587
yedidya ben haim <bhyedidya@gmail.com>
2026-06-24 11:01:39 +0300

    Implementing the LoadFile function

--- Program.cs
+++ Program.cs
@@ -1,12 +1,47 @@
 ﻿using System;
+using System.IO;
 
 namespace ReportFileAnalyzer
 {
     class Project
     {
+
+        
+        static string[] LoadFile(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                try
+                {
+                    string[] alllines = File.ReadAllLines(filePath);
+                    return alllines;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error reading file: {ex}");
+                    return new string[0];
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("File not found");
+                return new string[0];
+            }
+        }
+
+
+
+
+
+
         static void Main()
         {
-
+            string [] alllines = LoadFile("reports.txt");
+            foreach (string line in alllines)
+            {
+                Console.WriteLine(line);
+            }
         }
     }
 }
